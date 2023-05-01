391bbd91f69eebd3d61ad6befa4897fbd8d3597e
Juraj Martiček <marticekjuraj@gmail.com>
2023-05-01 13:36:25 +0200

    add colorized regions

--- xmlParser.py
+++ xmlParser.py
@@ -1,11 +1,13 @@
 from pero_ocr.document_ocr.layout import PageLayout
 import sys
-
+colors = ["31", "32", "33", "34", "35", "36", "37"]
 if __name__ == '__main__':
     # Create layout object
     layout = PageLayout(file = sys.argv[1])
     # Print layout object
     # print(layout)
+    colorIndex = 0
     for region in layout.regions:
-        print(region.get_region_transcription())
-        print(" ")
+        print("\x1b[" + colors[colorIndex%7] + "m", region.get_region_transcription())
+        colorIndex += 1
+    print("\n")

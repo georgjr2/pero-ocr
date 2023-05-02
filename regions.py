d67285c4e4ac63b0b6bbafcab000c5fbf4c814fe
Juraj Martiček <marticekjuraj@gmail.com>
2023-05-02 21:03:58 +0200

    publish regions from 3 and 4 questions 2022

---
+++ regions.py
@@ -0,0 +1,12 @@
+maxW = 2500
+q3minH = 170
+q3maxH = 450
+a3maxH = 1800
+question3 = [(0, q3minH), (maxW, q3minH), (maxW, q3maxH), (0, q3maxH), (0, q3minH)]
+answer3 =   [(0, q3maxH), (maxW, q3maxH), (maxW, a3maxH), (0, a3maxH), (0, q3maxH)]
+
+q4minH = 1810
+q4maxH = 2100
+a4maxH = 3600
+question4 = [(0, q4minH), (maxW, q4minH), (maxW, q4maxH), (0, q4maxH), (0, q4minH)]
+answer4 =   [(0, q4maxH), (maxW, q4maxH), (maxW, a4maxH), (0, a4maxH), (0, q4maxH)]

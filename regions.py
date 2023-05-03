d7f8507fe38283aaf63e07786141168bc93943c3
Patrik Tiszai <patotiszai@gmail.com>
2023-05-03 15:09:11 +0200

    added regions for test 2022 1.run

--- regions.py
+++ regions.py
@@ -22,3 +22,51 @@ q6maxH = 2270
 a6maxH = 3600
 question4 = [(0, q6minH), (maxW, q6minH), (maxW, q6maxH), (0, q6maxH), (0, q6minH)]
 answer4 =   [(0, q6maxH), (maxW, q6maxH), (maxW, a6maxH), (0, a6maxH), (0, q6maxH)]
+
+q7minH = 230
+q7maxH = 350
+a7maxH = 1960
+question7 = [(0, q7minH), (maxW, q7minH), (maxW, q7maxH), (0, q7maxH), (0, q7minH)]
+answer7 =   [(0, q7maxH), (maxW, q7maxH), (maxW, a7maxH), (0, a7maxH), (0, q7maxH)]
+
+q8minH = 1970
+q8maxH = 2080
+a8maxH = 3600
+question8 = [(0, q8minH), (maxW, q8minH), (maxW, q8maxH), (0, q8maxH), (0, q8minH)]
+answer8 =   [(0, q8maxH), (maxW, q8maxH), (maxW, a8maxH), (0, a8maxH), (0, q8maxH)]
+
+q9minH = 260
+q9maxH = 370
+a9maxH = 1850
+question9 = [(0, q9minH), (maxW, q9minH), (maxW, q9maxH), (0, q9maxH), (0, q9minH)]
+answer9 =   [(0, q9maxH), (maxW, q9maxH), (maxW, a9maxH), (0, a9maxH), (0, q9maxH)]
+
+q10minH = 1860
+q10maxH = 1950
+a10maxH = 2910
+question10 = [(0, q10minH), (maxW, q10minH), (maxW, q10maxH), (0, q10maxH), (0, q10minH)]
+answer10 =   [(0, q10maxH), (maxW, q10maxH), (maxW, a10maxH), (0, a10maxH), (0, q10maxH)]
+
+q11minH = 225
+q11maxH = 310
+a11maxH = 2010
+question11 = [(0, q11minH), (maxW, q11minH), (maxW, q11maxH), (0, q11maxH), (0, q11minH)]
+answer11 =   [(0, q11maxH), (maxW, q11maxH), (maxW, a11maxH), (0, a11maxH), (0, q11maxH)]
+
+q12minH = 2020
+q12maxH = 2110
+a12maxH = 2750
+question12 = [(0, q12minH), (maxW, q12minH), (maxW, q12maxH), (0, q12maxH), (0, q12minH)]
+answer12 =   [(0, q12maxH), (maxW, q12maxH), (maxW, a12maxH), (0, a12maxH), (0, q12maxH)]
+
+q13minH = 250
+q13maxH = 380
+a13maxH = 1870
+question13 = [(0, q13minH), (maxW, q13minH), (maxW, q13maxH), (0, q13maxH), (0, q13minH)]
+answer13 =   [(0, q13maxH), (maxW, q13maxH), (maxW, a13maxH), (0, a13maxH), (0, q13maxH)]
+
+q14minH = 1880
+q14maxH = 2020
+a14maxH = 3200
+question14 = [(0, q14minH), (maxW, q14minH), (maxW, q14maxH), (0, q14maxH), (0, q14minH)]
+answer14 =   [(0, q14maxH), (maxW, q14maxH), (maxW, a14maxH), (0, a14maxH), (0, q14maxH)]

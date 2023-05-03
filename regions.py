ad0f183af38b48ecb82d9c68e8e5cdd905534bd5
Juraj Martiček <marticekjuraj@gmail.com>
2023-05-03 13:39:50 +0200

    Add regions for q5 and q6

--- regions.py
+++ regions.py
@@ -10,3 +10,15 @@ q4maxH = 2100
 a4maxH = 3600
 question4 = [(0, q4minH), (maxW, q4minH), (maxW, q4maxH), (0, q4maxH), (0, q4minH)]
 answer4 =   [(0, q4maxH), (maxW, q4maxH), (maxW, a4maxH), (0, a4maxH), (0, q4maxH)]
+
+q5minH = 130
+q5maxH = 540
+a5maxH = 1980
+question3 = [(0, q5minH), (maxW, q5minH), (maxW, q5maxH), (0, q5maxH), (0, q5minH)]
+answer3 =   [(0, q5maxH), (maxW, q5maxH), (maxW, a5maxH), (0, a5maxH), (0, q5maxH)]
+
+q6minH = 2030
+q6maxH = 2270
+a6maxH = 3600
+question4 = [(0, q6minH), (maxW, q6minH), (maxW, q6maxH), (0, q6maxH), (0, q6minH)]
+answer4 =   [(0, q6maxH), (maxW, q6maxH), (maxW, a6maxH), (0, a6maxH), (0, q6maxH)]

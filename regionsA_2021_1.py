5fa4e4bd82d8ff68a598ebbe8adf598a5febe0e6
Patrik Tiszai <patotiszai@gmail.com>
2023-05-12 14:11:19 +0200

    regions for 2021 group A and B test-1

---
+++ regionsA_2021_1.py
@@ -0,0 +1,97 @@
+maxW = 2500
+
+q1minH = 380
+q1maxH = 530
+a1maxH = 2200
+question1 = [(0, q1minH), (maxW, q1minH), (maxW, q1maxH), (0, q1maxH), (0, q1minH)]
+answer1 =   [(0, q1maxH), (maxW, q1maxH), (maxW, a1maxH), (0, a1maxH), (0, q1maxH)]
+
+q2minH = 2210
+q2maxH = 2350  
+a2maxH = 3580
+question2 = [(0, q2minH), (maxW, q2minH), (maxW, q2maxH), (0, q2maxH), (0, q2minH)]
+answer2 =   [(0, q2maxH), (maxW, q2maxH), (maxW, a2maxH), (0, a2maxH), (0, q2maxH)]
+
+
+q3minH = 210
+q3maxH = 420
+a3maxH = 1890
+question3 = [(0, q3minH), (maxW, q3minH), (maxW, q3maxH), (0, q3maxH), (0, q3minH)]
+answer3 =   [(0, q3maxH), (maxW, q3maxH), (maxW, a3maxH), (0, a3maxH), (0, q3maxH)]
+
+q4minH = 1900
+q4maxH = 2130
+a4maxH = 3580
+question4 = [(0, q4minH), (maxW, q4minH), (maxW, q4maxH), (0, q4maxH), (0, q4minH)]
+answer4 =   [(0, q4maxH), (maxW, q4maxH), (maxW, a4maxH), (0, a4maxH), (0, q4maxH)]
+
+q5minH = 215
+q5maxH = 400
+a5maxH = 1950
+question5 = [(0, q5minH), (maxW, q5minH), (maxW, q5maxH), (0, q5maxH), (0, q5minH)]
+answer5 =   [(0, q5maxH), (maxW, q5maxH), (maxW, a5maxH), (0, a5maxH), (0, q5maxH)]
+
+q6minH = 1960
+q6maxH = 2130
+a6maxH = 3580
+question6 = [(0, q6minH), (maxW, q6minH), (maxW, q6maxH), (0, q6maxH), (0, q6minH)]
+answer6 =   [(0, q6maxH), (maxW, q6maxH), (maxW, a6maxH), (0, a6maxH), (0, q6maxH)]
+
+q7minH = 210
+q7maxH = 400
+a7maxH = 1960
+question7 = [(0, q7minH), (maxW, q7minH), (maxW, q7maxH), (0, q7maxH), (0, q7minH)]
+answer7 =   [(0, q7maxH), (maxW, q7maxH), (maxW, a7maxH), (0, a7maxH), (0, q7maxH)]
+
+q8minH = 1970
+q8maxH = 2120
+a8maxH = 3600
+question8 = [(0, q8minH), (maxW, q8minH), (maxW, q8maxH), (0, q8maxH), (0, q8minH)]
+answer8 =   [(0, q8maxH), (maxW, q8maxH), (maxW, a8maxH), (0, a8maxH), (0, q8maxH)]
+
+q9minH = 220
+q9maxH = 305
+a9maxH = 1790
+question9 = [(0, q9minH), (maxW, q9minH), (maxW, q9maxH), (0, q9maxH), (0, q9minH)]
+answer9 =   [(0, q9maxH), (maxW, q9maxH), (maxW, a9maxH), (0, a9maxH), (0, q9maxH)]
+
+q10minH = 1800
+q10maxH = 1950
+a10maxH = 3100
+question10 = [(0, q10minH), (maxW, q10minH), (maxW, q10maxH), (0, q10maxH), (0, q10minH)]
+answer10 =   [(0, q10maxH), (maxW, q10maxH), (maxW, a10maxH), (0, a10maxH), (0, q10maxH)]
+
+q11minH = 200
+q11maxH = 320
+a11maxH = 1810
+question11 = [(0, q11minH), (maxW, q11minH), (maxW, q11maxH), (0, q11maxH), (0, q11minH)]
+answer11 =   [(0, q11maxH), (maxW, q11maxH), (maxW, a11maxH), (0, a11maxH), (0, q11maxH)]
+
+q12minH = 1820
+q12maxH = 1950
+a12maxH = 3600
+question12 = [(0, q12minH), (maxW, q12minH), (maxW, q12maxH), (0, q12maxH), (0, q12minH)]
+answer12 =   [(0, q12maxH), (maxW, q12maxH), (maxW, a12maxH), (0, a12maxH), (0, q12maxH)]
+
+q13minH = 240
+q13maxH = 340
+a13maxH = 1840
+question13 = [(0, q13minH), (maxW, q13minH), (maxW, q13maxH), (0, q13maxH), (0, q13minH)]
+answer13 =   [(0, q13maxH), (maxW, q13maxH), (maxW, a13maxH), (0, a13maxH), (0, q13maxH)]
+
+q14minH = 1850
+q14maxH = 1960
+a14maxH = 3300
+question14 = [(0, q14minH), (maxW, q14minH), (maxW, q14maxH), (0, q14maxH), (0, q14minH)]
+answer14 =   [(0, q14maxH), (maxW, q14maxH), (maxW, a14maxH), (0, a14maxH), (0, q14maxH)]
+
+
+template = [
+    [question1, answer1, question2, answer2], 
+    [question3, answer3, question4, answer4],
+    [question5, answer5, question6, answer6], 
+    [question7, answer7, question8, answer8], 
+    [question9, answer9, question10, answer10],
+    [question11, answer11, question12, answer12],
+    [question13, answer13, question14, answer14],
+]
